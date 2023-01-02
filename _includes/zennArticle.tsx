856e02c2396979e447b0b9b1f7ca9f8a1fc318b9
Omochice <mochice.mls.ntl@gmail.com>
2023-01-03 01:05:02 +0900

    :muscle: update header

--- _includes/zennArticle.tsx
+++ _includes/zennArticle.tsx
@@ -1,22 +1,13 @@
 import { SharedHead } from "./components/sharedHead.tsx";
+import { ZennHeader } from "./components/zennHeader.tsx";
 
 export default ({ title, children, type }) => (
   <>
     <html lang="ja">
       <head>
         <SharedHead />
+        <ZennHeader />
         <title>{title}</title>
-        <link
-          rel="stylesheet"
-          href="https://esm.sh/zenn-content-css@0.1.134?css"
-        />
-        <script src="https://embed.zenn.studio/js/listen-embed-event.js" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.css"
-          integrity="sha384-Juol1FqnotbkyZUT5Z7gUPjQ9gzlwCENvUZTpQBAPxtusdwFLRy382PSDx5UUJ4/"
-          crossOrigin="anonymous"
-        />
       </head>
       <body>{children}</body>
     </html>
