946bbda1a13575e770c228398ab68c82194cd2c1
Omochice <mochice.mls.ntl@gmail.com>
2023-01-02 14:03:19 +0900

    :+1: add plugins

---
+++ plugins/zenn-renderer/mod.ts
@@ -0,0 +1,61 @@
+import type { Data, Engine, Helper, Page, Site } from "./deps.ts";
+import { loader, merge } from "./deps.ts";
+import { markdownToHtml } from "./deps.ts";
+
+export interface Options {
+  extensions: string[];
+  keepDefaultPlugins: boolean;
+}
+
+export const defaults: Options = {
+  extensions: [".md"],
+  keepDefaultPlugins: false,
+};
+
+export class MarkdownEngine implements Engine {
+  constructor() {}
+
+  deleteCache() {}
+
+  async render(
+    content: string,
+    _data?: Data,
+    _filename?: string,
+  ): Promise<string> {
+    return await Promise.resolve(markdownToHtml(content));
+  }
+
+  renderSync(
+    content: string,
+    _data?: Data,
+    _filename?: string,
+  ): string {
+    return markdownToHtml(content);
+  }
+
+  addHelper() {}
+}
+
+export default function (userOptions?: Partial<Options>) {
+  const options = merge(defaults, userOptions);
+
+  return function (site: Site) {
+    const engine = new MarkdownEngine();
+
+    site.loadPages(options.extensions, loader, engine);
+
+    function filter(string: string, _inline = false): string {
+      return engine.renderSync(string?.toString() || "").trim();
+    }
+
+    site.filter("md", filter as Helper);
+  };
+}
+
+export interface ZennPageData extends Page {
+  title: string;
+  emoji: string;
+  type: "tech" | "idea";
+  topics: string[];
+  published: boolean;
+}
