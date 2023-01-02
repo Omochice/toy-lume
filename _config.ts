1a44bf6287225c67cd1191aa6578cc750f576d4c
Omochice <mochice.mls.ntl@gmail.com>
2023-01-03 00:54:24 +0900

    :gear: add setting

---
+++ _config.ts
@@ -0,0 +1,14 @@
+import lume from "lume/mod.ts";
+import jsx from "lume/plugins/jsx.ts";
+import windi from "lume/plugins/windi_css.ts";
+import zennRenderer from "./plugins/zenn-renderer/mod.ts";
+import zennKatex from "./plugins/zenn-katex/mod.ts";
+
+const site = lume();
+
+site.use(jsx())
+  .use(windi())
+  .use(zennRenderer())
+  .use(zennKatex());
+
+export default site;
