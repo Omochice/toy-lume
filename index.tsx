d3c85e88760901cec394c7ccb705db3517b26d0e
Omochice <mochice.mls.ntl@gmail.com>
2023-01-27 17:00:47 +0900

    :+1: add css class

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import type { Data, Page } from "lume/core.ts";
 import { compareDesc, fromUnixTime } from "date-fns";
 import { SharedHead } from "./_includes/components/sharedHead.tsx";
 
-export const layout = "zenn.tsx";
+export const layout = "base.tsx";
 
 function comparePage(a: Page, b: Page): number {
   const defaultCreated = fromUnixTime(0);
@@ -20,7 +20,7 @@ const BlogPosts = () => {
 
   return (
     <>
-      <ul>
+      <ul className="leading-relaxed">
         {blogPosts.map((page) => (
           <li key={page.data.title}>
             <div className="flex">
@@ -31,16 +31,15 @@ const BlogPosts = () => {
               >
                 <a
                   href={page.data.url}
+                  className="text-blue-500 cursor-pointer"
                 >
                   {page.data.title}
                 </a>
               </div>
 
-              <div name="page-metadata">
-                {(page.data.topics ?? []).map((topic: string) => (
-                  <span>
-                    {topic}
-                  </span>
+              <div name="page-metadata" className="flex gap-1">
+                {(page.data.topics ?? []).filter(e => e.length !== 0).map((topic: string) => (
+                  <a className="bg-gray-300 rounded px-2 font-mono h-11/12 cursor-pointer">{topic.toLowerCase()}</a>
                 ))}
               </div>
             </div>
@@ -54,7 +53,7 @@ const BlogPosts = () => {
 export default (_data: Data) => {
   return (
     <>
-      <h1>Recent posts</h1>
+      <h1 className="text-6xl leading-normal">Recent posts</h1>
       <BlogPosts />
     </>
   );
